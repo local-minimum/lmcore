ddb2ef9ca83c0b4f9e87eaa36d1a19f70fda4893
Martin Zackrisson <martin.zackrisson@fripost.org>
2024-06-02 12:16:59 +0200

    Hijack Tile:Class for naming the tiles

---
+++ Assets/Tiled/Mapping.tsx
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Mapping" tilewidth="32" tileheight="32" tilecount="128" columns="16">
+ <image source="MappingSpriteSheet.png" width="512" height="256"/>
+ <tile id="1" type="Floor - No Walls"/>
+ <tile id="2" type="Floor - Walls W"/>
+ <tile id="3" type="Floor - Walls N"/>
+ <tile id="4" type="Floor - Walls E"/>
+ <tile id="5" type="Floor - Walls S"/>
+ <tile id="6" type="Floor - Walls WE"/>
+ <tile id="7" type="Floor - Walls NS"/>
+ <tile id="8" type="Floor - Walls NW"/>
+ <tile id="9" type="Floor - Walls NE"/>
+ <tile id="10" type="Floor - Walls SE"/>
+ <tile id="11" type="Floor - Walls SW"/>
+ <tile id="12" type="Floor - Walls SWE"/>
+ <tile id="13" type="Floor - Walls NSE"/>
+ <tile id="14" type="Floor - Walls NSW"/>
+ <tile id="15" type="Floor - Walls NWE"/>
+ <tile id="16" type="Floor - Walls NSWE"/>
+ <tile id="17" type="No Floor - No Walls"/>
+ <tile id="18" type="No Floor - Walls W"/>
+ <tile id="19" type="No Floor - Walls N"/>
+ <tile id="20" type="No Floor - Walls E"/>
+ <tile id="21" type="No Floor - Walls S"/>
+ <tile id="22" type="No Floor - Walls WE"/>
+ <tile id="23" type="No Floor - Walls NS"/>
+ <tile id="24" type="No Floor - Walls NW"/>
+ <tile id="25" type="No Floor - Walls NE"/>
+ <tile id="26" type="No Floor - Walls SE"/>
+ <tile id="27" type="No Floor - Walls SW"/>
+ <tile id="28" type="No Floor - Walls SWE"/>
+ <tile id="29" type="No Floor - Walls NSE"/>
+ <tile id="30" type="No Floor - Walls NSW"/>
+ <tile id="31" type="No Floor - Walls NWE"/>
+ <tile id="32" type="No Floor - Walls NSWE"/>
+ <tile id="33" type="Door - Walls WE"/>
+ <tile id="34" type="Grate - Walls WE"/>
+ <tile id="35" type="Door With Lock - Walls WE"/>
+ <tile id="36" type="Obstruction - Walls WE"/>
+ <tile id="37" type="Door - Walls NS"/>
+ <tile id="38" type="Grate - Walls NS"/>
+ <tile id="39" type="Door With Lock - Walls NS"/>
+ <tile id="40" type="Obstruction - Walls NS"/>
+ <tile id="42" type="Ramp Up - Exit S -&gt; N"/>
+ <tile id="43" type="Ramp Up - Entry N -&gt; S"/>
+ <tile id="44" type="Ramp Up - Exit W -&gt; E"/>
+ <tile id="45" type="Ramp Up - Mid W -&gt; E"/>
+ <tile id="46" type="Ramp Up - Entry W -&gt; E"/>
+ <tile id="48" type="Stairs Down - Entry S"/>
+ <tile id="49" type="Stairs Down - Entry W"/>
+ <tile id="50" type="Stairs Down - Entry N"/>
+ <tile id="51" type="Stairs Down - Entry E"/>
+ <tile id="52" type="Stairs Up - Entry S"/>
+ <tile id="53" type="Stairs Up - Entry W"/>
+ <tile id="54" type="Stairs Up - Entry N"/>
+ <tile id="55" type="Stairs Up - Entry E"/>
+ <tile id="56" type="Trapdoor - Split NS"/>
+ <tile id="57" type="Trapdoor - Split WE"/>
+ <tile id="58" type="Ramp Up - Mid S -&gt; N"/>
+ <tile id="59" type="Ramp Up - Mid N -&gt; S"/>
+ <tile id="60" type="Ramp Up - Entry E -&gt; W"/>
+ <tile id="61" type="Ramp Up - Mid E -&gt; W"/>
+ <tile id="62" type="Ramp Up - Exit E -&gt; W"/>
+ <tile id="64" type="Alcove - Face S"/>
+ <tile id="65" type="Alcove - Face W"/>
+ <tile id="66" type="Alcove - Face N"/>
+ <tile id="67" type="Alcove - Face E"/>
+ <tile id="68" type="Pillar"/>
+ <tile id="69" type="Pedistal"/>
+ <tile id="70" type="Moat NS"/>
+ <tile id="71" type="Moat WE"/>
+ <tile id="72" type="Moat SE"/>
+ <tile id="73" type="Moat SW"/>
+ <tile id="74" type="Ramp Up - Entry S -&gt; N"/>
+ <tile id="75" type="Ramp Up - Exit N -&gt; S"/>
+ <tile id="80" type="Chest"/>
+ <tile id="81" type="Chest With Lock"/>
+ <tile id="82" type="Fireplace"/>
+ <tile id="83" type="NPC"/>
+ <tile id="84" type="Enemy"/>
+ <tile id="88" type="Moat NE"/>
+ <tile id="89" type="Moat NW"/>
+ <tile id="96" type="Hole"/>
+ <tile id="97" type="Spinner - CCW"/>
+ <tile id="98" type="Spinner - CW"/>
+ <tile id="99" type="Button - Wall E"/>
+ <tile id="100" type="Button - Wall S"/>
+ <tile id="101" type="Button - Wall W"/>
+ <tile id="102" type="Button - Wall N"/>
+ <tile id="103" type="Teleporter - Entry"/>
+ <tile id="104" type="Teleporter - Entry/Exit"/>
+ <tile id="105" type="Teleporter - Exit"/>
+ <tile id="106" type="Pressureplate"/>
+ <tile id="107" type="Ladder - Wall N"/>
+ <tile id="108" type="Ladder - Wall E"/>
+ <tile id="109" type="Ladder - Wall S"/>
+ <tile id="110" type="Ladder - Wall W"/>
+ <tile id="112" type="Spikes - Floor"/>
+ <tile id="113" type="Spikes - Wall W"/>
+ <tile id="114" type="Spikes - Wall N"/>
+ <tile id="115" type="Spikes - Wall E"/>
+ <tile id="116" type="Spikes - Wall S"/>
+ <tile id="117" type="Fireball Shooter - Wall S"/>
+ <tile id="118" type="Fireball Shooter - Wall W"/>
+ <tile id="119" type="Fireball Shooter - Wall N"/>
+ <tile id="120" type="Fireball Shooter - Wall E"/>
+</tileset>
